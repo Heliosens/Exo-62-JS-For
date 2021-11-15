a3caf9bda940ab055a216d210a1cb52e8ca2a7b6
Sylvie-B <unsolo@hotmail.fr>
2021-11-15 21:58:34 +0100

    boucle * 5 pas de 2

--- exo 4/exercice4.js
+++ exo 4/exercice4.js
@@ -1,8 +1,8 @@
 /* Modifier le code ci dessous pour réaliser l'exercice */
 
-for(var i=2;   ; )
+for(var i=2; i < 12  ; i += 2 )
 {
-    document.getElementById('monDiv').innerHTML+="Ma variable i vaux "+i+"<br><br>";
+    document.getElementById('monDiv').innerHTML += "Ma variable i vaux " + i + "<br><br>";
 }
 
 
